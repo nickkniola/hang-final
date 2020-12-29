6e70355202cf46b37c300e476dd892a2f737e2a1
Nicholas Kniola <nickkniola@me.com>
2020-12-29 21:38:34 +0000

    Create modal which renders text and image from state

--- client/pages/select-activity.jsx
+++ client/pages/select-activity.jsx
@@ -33,6 +33,9 @@ export default class SelectActivity extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    this.setState({
+      activeView: 'acceptReject'
+    });
     // GET request to backend server checking if a matching activity exists
     const formData = this.state;
     fetch('/api/activities', {
@@ -60,32 +63,35 @@ export default class SelectActivity extends React.Component {
         if (!this.state.responseLocation && !this.state.activityObject) {
           // eslint-disable-next-line no-console
           console.log('No matching activity found. Please try again.');
-          return;
         }
-        let activityAction = 'Eat at';
-        let firstName = this.state.activityObject.firstName;
-        let location = this.state.activityObject.location;
-        if (this.state.responseLocation) {
-          firstName = 'Another User';
-          location = this.state.responseLocation.name;
-        }
-        if (this.state.activityType === 'Sports') {
-          if (this.state.preferredActivity) {
-            activityAction = `Play ${this.state.preferredActivity} at`;
-          } else {
-            activityAction = 'Play at';
-          }
-        } else if (this.state.activityType === 'Museum') {
-          activityAction = 'Visit';
-        }
-        // eslint-disable-next-line no-console
-        console.log(`${this.state.activityType} with ${firstName}. ${activityAction} ${location} on ${this.state.date} at 1PM.`);
       })
       .catch(() => console.error('An unexpected error occurred'));
   }
 
   render() {
     const activeView = this.state.activeView;
+    const activityObject = this.state.activityObject;
+    const responseLocation = this.state.responseLocation;
+    const activityType = this.state.activityType;
+    const date = this.state.date;
+    let activityAction = 'Eat at';
+    let firstName = this.state.activityObject.firstName;
+    let location = this.state.activityObject.location;
+    let profileImage = this.state.activityObject.profileImage;
+    if (this.state.responseLocation) {
+      firstName = 'Another User';
+      location = this.state.responseLocation.name;
+      profileImage = 'https://semantic-ui.com/images/avatar2/large/molly.png';
+    }
+    if (this.state.activityType === 'Sports') {
+      if (this.state.preferredActivity) {
+        activityAction = `Play ${this.state.preferredActivity} at`;
+      } else {
+        activityAction = 'Play at';
+      }
+    } else if (this.state.activityType === 'Museum') {
+      activityAction = 'Visit';
+    }
     return (
       <>
         <div className="ui menu">
@@ -93,62 +99,113 @@ export default class SelectActivity extends React.Component {
             <img src="/images/hang_logo.png"></img>
           </a>
           <a className="item" onClick={this.handleMenuClick}>
-            Pairing
+          Pairing
           </a>
           <a className="item" onClick={this.handleMenuClick}>
-            Random
+          Random
           </a>
         </div>
-        <h2 className="ui header secondary-header">
-          {activeView === 'Pairing' ? 'Select Activity' : 'Random Activity'}
-        </h2>
-        <div className="ui segment">
-          <form className="ui form" onSubmit={this.handleSubmit}>
-            <div className="two fields">
-              <div className="field">
-                <label htmlFor="city" >City</label>
-                <input type="text" name="city" id="city" placeholder="ex. Chicago" value={this.state.city} onChange={this.handleChange} />
+        { activeView === 'acceptReject'
+          ? <>
+              <div className="ui card centered">
+              { !activityObject && !responseLocation
+                ? <div className="ui placeholder">
+                    <div className="content">
+                      <div className="ui placeholder">
+                        <div className="square image"></div>
+                        <div className="ui active inverted dimmer">
+                          <div className="ui massive text loader">Loading</div>
+                        </div>
+                        <div className="header">
+                          <div className="line"></div>
+                        </div>
+                        <div className="paragraph">
+                          <div className="line"></div>
+                          <div className="line"></div>
+                        </div>
+                        <div className="paragraph">
+                          <div className="line"></div>
+                          <div className="line"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                : <>
+                    <div className="image">
+                      <img src={profileImage} />
+                    </div>
+                    <div className="content">
+                      <div className="header">{activityType} with {firstName}</div>
+                      <div className="description">
+                        {activityAction} <a href="#">{location}</a> with <span className="name-text">{firstName}</span> on {date} at 1PM.
+                      </div>
+                    </div>
+                    <div className="extra content">
+                      <div className='ui two buttons'>
+                        <button className="ui primary button" type="button">
+                        Accept
+                        </button>
+                        <button className="ui red button" type="button" onClick={this.handleSubmit}>
+                        Reject
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                }
               </div>
-              <div className="field">
-                <label htmlFor="neighborhood" >Neighborhood</label>
-                <input type="text" name="neighborhood" id="neighborhood" placeholder="ex. Uptown" value={this.state.neighborhood} onChange={this.handleChange} />
-              </div>
-            </div>
-            <div className="field">
-              <label htmlFor="state">State</label>
-              <input type="text" name="state" id="state" placeholder="ex. Illinois" value={this.state.state} onChange={this.handleChange} />
-            </div>
-            <div className="field">
-              <label htmlFor="date">Date</label>
-              <input type="date" name="date" id="date" placeholder="yyyy-mm-dd"
-                value={this.state.date} onChange={this.handleChange}
-                min="2021-01-01" max="2030-01-01" />
-            </div>
-
-            { activeView === 'Pairing' &&
-              <>
-                <div className="field">
-                  <label htmlFor="activityType">Activity Type</label>
-                  <select type="menu" name="activityType" id="activityType" value={this.state.activityType} onChange={this.handleChange} required >
-                    <option value="">Select Activity...</option>
-                    <option name="food" value="Food">Food</option>
-                    <option name="museum" value="Museum">Museum</option>
-                    <option name="sports" value="Sports">Sports</option>
-                  </select>
+            </>
+          : <div>
+            <h2 className="ui header secondary-header">
+              {activeView === 'Pairing' ? 'Select Activity' : 'Random Activity'}
+            </h2>
+            <div className="ui segment">
+              <form className="ui form" onSubmit={this.handleSubmit}>
+                <div className="two fields">
+                  <div className="field">
+                    <label htmlFor="city" >City</label>
+                    <input type="text" name="city" id="city" placeholder="ex. Chicago" value={this.state.city} onChange={this.handleChange} />
+                  </div>
+                  <div className="field">
+                    <label htmlFor="neighborhood" >Neighborhood</label>
+                    <input type="text" name="neighborhood" id="neighborhood" placeholder="ex. Uptown" value={this.state.neighborhood} onChange={this.handleChange} />
+                  </div>
                 </div>
                 <div className="field">
-                  <label htmlFor="preferredActivity" >Preferred Activity</label>
-                  <input type="text" name="preferredActivity" id="preferredActivity" placeholder="ex. Tennis" value={this.state.preferredActivity} onChange={this.handleChange} />
+                  <label htmlFor="state">State</label>
+                  <input type="text" name="state" id="state" placeholder="ex. Illinois" value={this.state.state} onChange={this.handleChange} />
                 </div>
-              </>
-            }
+                <div className="field">
+                  <label htmlFor="date">Date</label>
+                  <input type="date" name="date" id="date" placeholder="yyyy-mm-dd"
+                    value={this.state.date} onChange={this.handleChange}
+                    min="2021-01-01" max="2030-01-01" />
+                </div>
 
-            <button className="ui primary button" type='submit'>
-              {activeView === 'Pairing' ? 'Submit' : 'Randomize'}
-            </button>
-          </form>
-        </div>
+                { activeView === 'Pairing' &&
+                  <>
+                    <div className="field">
+                      <label htmlFor="activityType">Activity Type</label>
+                      <select type="menu" name="activityType" id="activityType" value={this.state.activityType} onChange={this.handleChange} required >
+                        <option value="">Select Activity...</option>
+                        <option name="food" value="Food">Food</option>
+                        <option name="museum" value="Museum">Museum</option>
+                        <option name="sports" value="Sports">Sports</option>
+                      </select>
+                    </div>
+                    <div className="field">
+                      <label htmlFor="preferredActivity" >Preferred Activity</label>
+                      <input type="text" name="preferredActivity" id="preferredActivity" placeholder="ex. Tennis" value={this.state.preferredActivity} onChange={this.handleChange} />
+                    </div>
+                  </>
+                }
 
+                <button className="ui primary button" type='submit'>
+                  {activeView === 'Pairing' ? 'Submit' : 'Randomize'}
+                </button>
+              </form>
+            </div>
+          </div>
+        }
       </>
     );
   }
