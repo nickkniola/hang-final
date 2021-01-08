d9f6ba2ceca88d3730b68ceef1acf96eea051e2a
Nicholas Kniola <nickkniola@me.com>
2021-01-08 01:50:27 +0000

    Store form data in state onChange

--- client/pages/SignUp.jsx
+++ client/pages/SignUp.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="sign-up-container">
@@ -9,19 +26,19 @@ export default class SignUp extends React.Component {
             <h2 className="secondary-header">Sign Up</h2>
             <div className="field">
               <label htmlFor="firstName">First Name</label>
-              <input type="text" name="firstName" id="firstName" placeholder="First Name" required />
+              <input type="text" name="firstName" id="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange} required />
             </div>
             <div className="field">
               <label htmlFor="name">Last Name</label>
-              <input type="text" name="lastName" id="lastName" placeholder="Last Name" required />
+              <input type="text" name="lastName" id="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} required />
             </div>
             <div className="field">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" placeholder="Email" required />
+              <input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
             </div>
             <div className="field">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" placeholder="Password" autoComplete="on" required />
+              <input type="password" name="password" id="password" placeholder="Password" autoComplete="on" value={this.state.password} onChange={this.handleChange} required />
             </div>
             <div className="button-container sign-up">
               <button className="large ui primary button sign-up-button" type="submit">Create Account</button>
